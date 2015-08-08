df7cc52efb7a9f5d47cba13a10803f4491ce8e37
Randy Eckman <emanspeaks@gmail.com>
2015-08-08 12:45:59 -0500

    removed try from mount

--- prep/mount.sh
+++ prep/mount.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-try mount -v -t ext4 /dev/md0p2 /mnt/lfs || /
-  (echo "LFS failed to mount: $*"; exit 1)
+mount -v -t ext4 /dev/md0p2 /mnt/lfs || echo "LFS failed to mount: $*" && exit 1)
