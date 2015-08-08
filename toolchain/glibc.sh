fb404e6afdfd49eb929ac6036fe82c1584cb808a
Randy Eckman <emanspeaks@gmail.com>
2015-08-08 14:04:21 -0500

    added glibc

---
+++ toolchain/glibc.sh
@@ -0,0 +1,25 @@
+if [ ! -r /usr/include/rpc/types.h ]; then
+  try su -c 'mkdir -pv /usr/include/rpc'
+  try su -c 'cp -v sunrpc/rpc/*.h /usr/include/rpc'
+fi
+try sed -e '/ia32/s/^/1:/' \
+    -e '/SSE2/s/^1://' \
+    -i  sysdeps/i386/i686/multiarch/mempcpy_chk.S
+try mkdir -v ../glibc-build
+cd ../glibc-build
+
+try ../glibc-2.21/configure                             \
+      --prefix=/tools                               \
+      --host=$LFS_TGT                               \
+      --build=$(../glibc-2.21/scripts/config.guess) \
+      --disable-profile                             \
+      --enable-kernel=2.6.32                        \
+      --with-headers=/tools/include                 \
+      libc_cv_forced_unwind=yes                     \
+      libc_cv_ctors_header=yes                      \
+      libc_cv_c_cleanup=yes
+try make
+try make install
+
+cd ..
+try rm -vrf glibc-build
