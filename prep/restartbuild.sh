69aa38948594c87a03d8e8bde83e237bc5e0fc3b
Randy Eckman <emanspeaks@gmail.com>
2015-08-09 14:10:33 -0500

    removing sudo

--- prep/restartbuild.sh
+++ prep/restartbuild.sh
@@ -1,2 +1,4 @@
 #!/tools/bin/bash
-sudo rm -rf bin  boot  etc  home  lib  lib64  media  mnt  opt  root  sbin  srv  usr  var  
+#run as root
+cd /mnt/lfs
+rm -rf bin  boot  etc  home  lib  lib64  media  mnt  opt  root  sbin  srv  usr  var  
