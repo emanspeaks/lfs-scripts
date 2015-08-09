62c31fa304d70afcc39c4f24613a63dd1d9a6631
Randy Eckman <emanspeaks@gmail.com>
2015-08-09 01:07:58 -0500

    remove try from find in linuxapi

--- sysbuild/linuxapi.sh
+++ sysbuild/linuxapi.sh
@@ -1,4 +1,4 @@
 try make mrproper
 try make INSTALL_HDR_PATH=dest headers_install
-try find dest/include \( -name .install -o -name ..install.cmd \) -delete
+find dest/include \( -name .install -o -name ..install.cmd \) -delete
 try cp -rv dest/include/* /usr/include
