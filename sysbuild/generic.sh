3f3b90951b53cee058d5c7a66552828f3b5bfeb3
Randy Eckman <emanspeaks@gmail.com>
2015-08-09 03:40:21 -0500

    fixed find

--- sysbuild/generic.sh
+++ sysbuild/generic.sh
@@ -3,5 +3,5 @@ try make
 try make check
 try make DESTDIR=/usr/pkg/$pkgname/$tardir install
 try ln -s /usr/pkg/$pkgname/$tardir /usr/pkg/$pkgname/ver
-try find * -type d -exec mkdir -pv /{} \;
-try find * -type f -exec ln -svf /usr/pkg/$pkgname/ver/{} /{} \;
+find * -type d -exec mkdir -pv /{} \;
+find * -type f -exec ln -svf /usr/pkg/$pkgname/ver/{} /{} \;
