bdd43d0ae440624161f89ec237bd3d33779fcfd1
Randy Eckman <emanspeaks@gmail.com>
2015-08-09 13:15:35 -0500

    trying to fix broken symlinks

--- sysbuild/makeinstall.sh
+++ sysbuild/makeinstall.sh
@@ -1,7 +1,8 @@
 try make DESTDIR=/usr/pkg/$pkgname/$tardir install
 pkgpath=/usr/pkg/$pkgname
-try ln -svf $pkgpath/$tardir $pkgpath/ver
-pushd $pkgpath/ver
+pushd $pkgpath
+try ln -svf $tardir ver
+cd ver
 find * -type d -exec mkdir -pv /{} \;
 find * -type f -exec ln -svf $pkgpath/ver/{} /{} \;
 popd
