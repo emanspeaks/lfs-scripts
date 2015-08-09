a73297d423881636c340df62c52f53d4ddfdc87e
Randy Eckman <emanspeaks@gmail.com>
2015-08-09 00:09:48 -0500

    added LFS variable to mount.sh

--- prep/mount.sh
+++ prep/mount.sh
@@ -8,5 +8,6 @@ then
   source $lfsroot/include/include.sh
 fi
 
-try mount -v -t ext4 /dev/md0p2 /mnt/lfs
+export LFS=/mnt/lfs
+try mount -v -t ext4 /dev/md0p2 $LFS
 try mount -v --bind $lfsroot $LFS/sources/lfs-scripts
