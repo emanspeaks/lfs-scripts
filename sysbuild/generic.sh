c09256dc8f496f9674d9e8f1d86bc1720d4bbb5b
Randy Eckman <emanspeaks@gmail.com>
2015-08-09 03:42:37 -0500

    force overwriting symlinks

--- sysbuild/generic.sh
+++ sysbuild/generic.sh
@@ -2,6 +2,6 @@ try ./configure --prefix=/usr
 try make
 try make check
 try make DESTDIR=/usr/pkg/$pkgname/$tardir install
-try ln -s /usr/pkg/$pkgname/$tardir /usr/pkg/$pkgname/ver
+try ln -svf /usr/pkg/$pkgname/$tardir /usr/pkg/$pkgname/ver
 find * -type d -exec mkdir -pv /{} \;
 find * -type f -exec ln -svf /usr/pkg/$pkgname/ver/{} /{} \;
