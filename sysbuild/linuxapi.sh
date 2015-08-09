a695967a03dabb12fd53515183ed5ba2a8b37a37
Randy Eckman <emanspeaks@gmail.com>
2015-08-09 06:48:20 -0500

    fixed bad path in binutils

--- sysbuild/linuxapi.sh
+++ sysbuild/linuxapi.sh
@@ -1,7 +1,7 @@
 try make mrproper
-pkgpath=/usr/pkg/linuxapi/3.19
-try make INSTALL_HDR_PATH=$pkgpath headers_install
-find $pkgpath/include \( -name .install -o -name ..install.cmd \) -delete
+pkgpath=/usr/pkg/linuxapi
+try make INSTALL_HDR_PATH=$pkgpath/3.19 headers_install
+find $pkgpath/3.19/include \( -name .install -o -name ..install.cmd \) -delete
 try ln -svf $pkgpath/3.19 $pkgpath/ver
 pushd $pkgpath/ver/include
 find * -type d -exec mkdir -pv /usr/include/{} \;
