9c56c39da288d05c00830211d2049fee8dbea60e
Randy Eckman <emanspeaks@gmail.com>
2015-08-09 22:12:31 -0500

    missed a line in readline

--- sysbuild/readline.sh
+++ sysbuild/readline.sh
@@ -3,6 +3,7 @@ sed -i '/MV.*old/d' Makefile.in
 sed -i '/{OLDSUFF}/c:' support/shlib-install
 try ./configure --prefix=/usr --docdir=/usr/share/doc/readline-6.3
 try make SHLIB_LIBS=-lncurses
+try make SHLIB_LIBS=-lncurses install
 try mv -v /usr/lib/lib{readline,history}.so.* /lib
 try ln -sfv ../../lib/$(readlink /usr/lib/libreadline.so) /usr/lib/libreadline.so
 try ln -sfv ../../lib/$(readlink /usr/lib/libhistory.so ) /usr/lib/libhistory.so
