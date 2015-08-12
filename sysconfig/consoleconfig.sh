ddb43f5e03ed31645887bee1020ce193af975fc3
Randy Eckman <emanspeaks@gmail.com>
2015-08-12 17:56:21 -0500

    more console/sys config

---
+++ sysconfig/consoleconfig.sh
@@ -0,0 +1,3 @@
+try install -m 755 $lfsroot/sysconfig/templates/profile /etc/
+try install -m 755 $lfsroot/sysconfig/templates/inputrc /etc/
+try install -m 755 $lfsroot/sysconfig/templates/shells /etc/
