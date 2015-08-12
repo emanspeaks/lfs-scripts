ecd3e86354d00f57d8a24f0775bb306bdd59954b
Randy Eckman <emanspeaks@gmail.com>
2015-08-12 15:32:30 -0500

    small fix to sysconfig

--- sysconfig/sysconfig.sh
+++ sysconfig/sysconfig.sh
@@ -1,13 +1,13 @@
-bash /lib/udev/init-net-rules.sh
-cat /etc/udev/rules.d/70-persistent-net.rules
+try bash /lib/udev/init-net-rules.sh
+#cat /etc/udev/rules.d/70-persistent-net.rules
  
 # I want optical drive to by-path persistence since it will always
 # be on the same SATA port all the time and won't care if I swap devs
-udevadm test /sys/block/sr0
+#udevadm test /sys/block/sr0
 sed -i -e 's/"write_cd_rules"/"write_cd_rules by-path"/' \
-/etc/udev/rules.d/83-cdrom-symlinks.rules
+	/etc/udev/rules.d/83-cdrom-symlinks.rules
 
 # for me, eno1 is intranet, eno2 is server/internet
-install -m 755 $lfsroot/sysconfig/templates/ifconfig.* /etc/sysconfig/
-install -m 755 $lfsroot/sysconfig/templates/resolv.conf /etc/
-install -m 755 $lfsroot/sysconfig/templates/host* /etc/
+try install -m 755 $lfsroot/sysconfig/templates/ifconfig.* /etc/sysconfig/
+try install -m 755 $lfsroot/sysconfig/templates/resolv.conf /etc/
+try install -m 755 $lfsroot/sysconfig/templates/host* /etc/
