0ca407c23543d663335eb5f6d39ac0bcf13ceaa2
root <root@richmond.randyeckman.com>
2015-08-17 21:51:04 -0500

    removed 00_header and fixed mounts

--- prep/mount.sh
+++ prep/mount.sh
@@ -9,5 +9,9 @@ then
 fi
 
 export LFS=/mnt/lfs
-try mount -v -t ext4 /dev/md0p2 $LFS
+try mount -v -t ext4 /dev/syme/lfsroot $LFS
+try mount -v -t ext4 /dev/syme/var $LFS/var
+try mount -v -t ext4 /dev/syme/home $LFS/home
+try mount -v -t ext4 /dev/syme/tools $LFS/tools
+try mount -v -t ext4 /dev/sda2 $LFS/boot
 try mount -v --bind $lfsroot $LFS/sources/lfsscripts
