39659e0a9da8834bf387e9affcc2daf92cf59ec8
Randy Eckman <emanspeaks@gmail.com>
2015-08-17 23:42:00 -0500

    forgot shadow

--- main4.sh
+++ main4.sh
@@ -64,20 +64,21 @@ case $startpt in
 	7) ptry $wrap acl-2.2.52.src.tar.gz acl.sh ;&
 	8) ptry $wrap libcap-2.24.tar.xz libcap.sh ;&
 	9) ptry $wrap sed-4.2.2.tar.bz2 sed.sh ;&
-	10) ptry $wrap psmisc-22.21.tar.gz psmisc.sh ;&
-	11) ptry $wrap procps-ng-3.3.10.tar.xz procps-ng.sh ;&
-	12) ptry $wrap e2fsprogs-1.42.12.tar.gz e2fsprogs.sh ;&
-	13) ptry $wrap coreutils-8.23.tar.xz coreutils.sh ;&
-	14) ptry $wrap iana-etc-2.30.tar.bz2 iana-etc.sh ;&
-	15) ptry $wrap m4-1.4.17.tar.xz generic.sh ;&
-	16) ptry $wrap flex-2.5.39.tar.bz2 flex.sh \
+	10) ptry $wrap shadow-4.2.1.tar.xz shadow.sh ;&
+	11) ptry $wrap psmisc-22.21.tar.gz psmisc.sh ;&
+	12) ptry $wrap procps-ng-3.3.10.tar.xz procps-ng.sh ;&
+	13) ptry $wrap e2fsprogs-1.42.12.tar.gz e2fsprogs.sh ;&
+	14) ptry $wrap coreutils-8.23.tar.xz coreutils.sh ;&
+	15) ptry $wrap iana-etc-2.30.tar.bz2 iana-etc.sh ;&
+	16) ptry $wrap m4-1.4.17.tar.xz generic.sh ;&
+	17) ptry $wrap flex-2.5.39.tar.bz2 flex.sh \
 		--docdir=/usr/share/doc/flex-2.5.39 ;&
-	17) ptry $wrap bison-3.0.4.tar.xz generic.sh \
+	18) ptry $wrap bison-3.0.4.tar.xz generic.sh \
 		--docdir=/usr/share/doc/bison-3.0.4 ;&
-	18) ptry $wrap grep-2.21.tar.xz grep.sh --bindir=/bin ;&
-	19) ptry $wrap readline-6.3.tar.gz readline.sh ;&
+	19) ptry $wrap grep-2.21.tar.xz grep.sh --bindir=/bin ;&
+	20) ptry $wrap readline-6.3.tar.gz readline.sh ;&
 	
-	20) 
+	21) 
 		ptry $wrap bash-4.3.30.tar.gz bash.sh 
 		echo "RUN"
 		echo '"exec /bin/bash --login +h"'
