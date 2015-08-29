8e4fdcf6de7e7e94b83260aea75d4a9cbbb7bb88
emanspeaks <emanspeaks@gmail.com>
2015-08-29 12:59:00 -0500

    updated console settings

--- sysconfig/initconfig.sh
+++ sysconfig/initconfig.sh
@@ -1,2 +1,3 @@
 try install -v -m644 $lfsroot/sysconfig/templates/inittab /etc/
 try install -v -m644 $lfsroot/sysconfig/templates/clock /etc/sysconfig/
+try install -v -m644 $lfsroot/sysconfig/templates/console /etc/sysconfig/
